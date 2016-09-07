082d1021d08203b3183c95c91a7e452dcdc03344
ven3k <admin@ven3k.eu>
2016-09-07 12:21:45 +0200

    Update plagiator.js

--- plagiator.js
+++ plagiator.js
@@ -1,9 +1,15 @@
 function plagiat() {
-  console.log("wlaczyles funkcje")
-  var tresc = document.getElementById('plagiat').value
+  console.log("wlaczyles funkcje");
+  var tresc = document.getElementById('plagiat').value;
   var tablica = tresc.split(' ');
   var min = 1, max = tablica.length;
   var random = Math.floor(Math.random() * (max - min +1)) + min - 1;
-  console.log(random)
-  console.log(tablica[random])
+  console.log(random);
+  console.log(tablica[random]);
+  $.ajax({
+    url: "https://www.synonimy.pl/synonim/"+tablica[random]+"/",
+    success: function(result){
+      console.log(result.split("<dd class=\"last\">")[1].split("</dd>")[0]);
+    }
+  })
 }
