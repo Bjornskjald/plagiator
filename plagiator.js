c448555ed364205e9940f671cb4a7e43ef5d2e3b
Ven3k <admin@ven3k.eu>
2016-09-07 11:51:51 +0200

    Rozpoczęcie prac nad kodem

--- plagiator.js
+++ plagiator.js
@@ -1,4 +1,8 @@
 function plagiat() {
 console.log("wlaczyles funkcje")
 var tresc = document.getElementById('plagiat').value
+var tablica = tresc.split(' ');
+var min = 1, max = talica.count;
+var random = Math.floor(Math.random() * (max - min +1)) + min
+
 }
