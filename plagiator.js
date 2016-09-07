5609b053ad2c38216f2f55390c163bd0e81be06b
ven3k <admin@ven3k.eu>
2016-09-07 12:12:14 +0200

    Literówka

--- plagiator.js
+++ plagiator.js
@@ -2,7 +2,7 @@ function plagiat() {
   console.log("wlaczyles funkcje")
   var tresc = document.getElementById('plagiat').value
   var tablica = tresc.split(' ');
-  var min = 1, max = tablica.count;
+  var min = 1, max = tablica.length;
   var random = Math.floor(Math.random() * (max - min +1)) + min;
   console.log(random)
 
